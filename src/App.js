b674c86dd5f1cdb97cc74ce4e359f02a855cb821
Asish Jena <ashishkumararya1996@gmail.com>
2021-05-08 13:25:08 +0530

    001 First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  var name="Asish Jena";
+  return (
+    <div className="App">
+      <header className="App-header">
+       My name is {name}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+git config --global user.email "ashishkumararya1996@gmail.com"
+  git config --global user.name "Asish Jena"
